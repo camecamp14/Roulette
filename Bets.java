067ed80658a9c674cc4a9219e8bd35394a83ca69
Cameron Campbell <68791224+camecamp14@users.noreply.github.com>
2020-08-30 16:10:10 -0500

    Add files via upload

---
+++ Bets.java
@@ -0,0 +1,29 @@
+
+public class Bets {
+	int[] odd = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35};
+	int odd_pay = 1;
+	
+	int [] even = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36};
+	int even_pay = 1;
+	
+	int[] sixes = {1,2,3,4,5,6};
+	int sixes_pay = 5;
+	
+	int[] corners = {1,2,4,5};
+	int corner_pay = 8;
+	
+	int[] twelves = {1,2,3,4,5,6,7,8,9,10,11,12};
+	int twelves_pay = 2;
+	
+	int[] splits = {1,2};
+	int splits_pay = 17;
+	
+	int[] straight = {1};
+	int straight_pay = 35;
+	
+	//public static void main(String[] args) {
+		
+
+	//}
+
+}
