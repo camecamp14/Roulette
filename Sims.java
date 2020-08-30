c37e477dfce6d9bb27b5b7f565b15c5ff5543400
Cameron Campbell <68791224+camecamp14@users.noreply.github.com>
2020-08-30 16:09:39 -0500

    Update Sims.java

--- Sims.java
+++ Sims.java
@@ -64,5 +64,50 @@ public class Sims {
 		return probwin;
 		
 	}
+	/*
+	Simulates 1000 games to calculate the probability of winning before going bankrupt. A win
+	is decided by the player reaching a value or money equal to the start value times a multiplying factor. This method allows
+	for multiple bets. Each game is stopped when the player either runs out of money or reaches this winning total. The probabilty of winning
+	is calculated by the total number of wins over the total number of games.
+	
+	Params
+	bet: array of arrays containing values which correspond to player win. Each array is a single bet
+	wager: how much the player is wagering per bet
+	mult: multiplication factor to decide when player wins
+	payout: array corresponding to how much a win pays to player (Depends on bet)
+	start: amount of money the player starts with
+	*/*/
+	
+	public static double play_to_mult(int[][]bet,int wager, double mult, int payout[]
+			,int start) {
+		double probwin;
+		double wins=0;
+		int money;
+		int games = 1000;
+		for(int i=0;i<games;i++) {
+			money = start;
+			while((wager <= money) && (money <= (start*mult))){
+				int roll = roll();
+				for(int j =0;j<bet.length;j++) { //Loops to check if any of player's bets are winners
+					boolean win = check_win(roll,bet[j]);
+					if(win) {
+						money += (payout[j]*wager);	
+					}
+					else {
+						money -= wager;
+					}
+					
+				}
+				if (money>= (start*mult)) {
+					wins+=1;
+					break;
+					}
+			
+				}
+			}
+		probwin = wins/games;
+		return probwin;
+		
+	}
 
 }
