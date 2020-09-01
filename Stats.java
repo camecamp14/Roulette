2d0c656483950278f098fb8e3b9e77c2682778bf
Cameron Campbell <68791224+camecamp14@users.noreply.github.com>
2020-09-01 17:54:58 -0500

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -14,11 +14,36 @@ public class Stats {
 	}
 	
 	/*
+	Allows for multiple bets
+	*/
+	public static double expected_value(int[][] bet, int wager, double mult, int payout[],
+	int start) {
+		double exp_value;
+		double prob = Sims.play_to_mult(bet, wager, mult, payout, start);
+		exp_value = prob*start;
+		return exp_value;	
+	}
+	
+	/*
 	Calculated the standard deviation in amount won while playing to a specific amount with a specified wager and bet.
 	Standard deviation is calculated using the defintion of variance as the expectation of the win probabilty squared 
 	minus the squre of the expectation value.
 	*/
 	public static double calc_sd(int[] bet, int wager, double mult, int payout,
+	int start) {
+		double var;
+		double exp_value_sqrd;
+		double prob = Sims.play_to_mult(bet, wager, mult, payout, start);
+		exp_value_sqrd = (prob*prob)*start;
+		double exp_value = expected_value(bet, wager, mult, payout, start);
+		var = (exp_value*exp_value)-exp_value_sqrd;
+		return (Math.sqrt(var))
+	}
+	
+	/*
+	Allows for multiple bets
+	*/
+	public static double calc_sd(int[][] bet, int wager, double mult, int[] payout,
 	int start) {
 		double var;
 		double exp_value_sqrd;
