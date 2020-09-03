fef3cf9966b76cbcbb6f6eacd8296548a5cf4f8c
Cameron Campbell <68791224+camecamp14@users.noreply.github.com>
2020-09-03 17:26:54 -0500

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -20,24 +20,25 @@ public class Stats {
 	int start) {
 		double exp_value;
 		double prob = Sims.play_to_mult(bet, wager, mult, payout, start);
-		exp_value = prob*start;
+		exp_value = prob*start*mult;
 		return exp_value;	
 	}
 	
 	/*
 	Calculated the standard deviation in amount won while playing to a specific amount with a specified wager and bet.
-	Standard deviation is calculated using the defintion of variance as the expectation of the win probabilty squared 
-	minus the squre of the expectation value.
+	Standard deviation is calculated using the definition of variance as the expectation of the win probability squared 
+	minus the square of the expectation value.
 	*/
 	public static double calc_sd(int[] bet, int wager, double mult, int payout,
 	int start) {
 		double var;
 		double exp_value_sqrd;
 		double prob = Sims.play_to_mult(bet, wager, mult, payout, start);
-		exp_value_sqrd = (prob*prob)*start;
+		exp_value_sqrd = prob*(start*mult)*(start*mult);
 		double exp_value = expected_value(bet, wager, mult, payout, start);
-		var = (exp_value*exp_value)-exp_value_sqrd;
-		return (Math.sqrt(var))
+		var = exp_value_sqrd-(exp_value*exp_value);
+		System.out.println(exp_value_sqrd);
+		return (Math.sqrt(var));
 	}
 	
 	/*
@@ -51,7 +52,7 @@ public class Stats {
 		exp_value_sqrd = (prob*prob)*start;
 		double exp_value = expected_value(bet, wager, mult, payout, start);
 		var = (exp_value*exp_value)-exp_value_sqrd;
-		return (Math.sqrt(var))
+		return (Math.sqrt(var));
 	}
 	/*
 	Integrates using left handed rectangle method. Creates an array of x values spanning from the lower bound to upper bound.
@@ -69,13 +70,13 @@ public class Stats {
 		double [] func = normal_pdf(x,mu,sigma);
 		
 		for(int i=0;i<len_x;i++) {
-			result += x[i]*inc;
+			result += func[i]*inc;
 		}
 		
 		return result;
 	}
 	/*
-	Coverts x array into the normal pdf for integration.
+	Converts x array into the normal pdf for integration.
 	*/
 	public static double[] normal_pdf(double[] x, double mu,double sigma) {
 		double[] y = new double[x.length];
